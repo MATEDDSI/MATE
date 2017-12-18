20cb2882ff9dc999f26fab18a01f35c373f8c8ea
InakiMadi <inakimadi@gmail.com>
2017-12-18 06:05:48 +0100

    Atletismo añadido

--- datos.sql
+++ datos.sql
@@ -1,9 +1,9 @@
 INSERT INTO Juego (nombre) VALUES ('Hearthstone'), ('Pokemon'), ('Atletismo');
 
-INSERT INTO Atributo (nombre, tipo) VALUES ('numero de cartas', 'int'), ('primero', 'string'), ('tiempo', 'string');
+INSERT INTO Atributo (nombre, tipo) VALUES ('numero de cartas', 'int'), ('primero', 'string'), ('prueba', 'string');
 
 INSERT INTO tiene (nombreJuego, nombreAtributo)
-VALUES ('Hearthstone', 'numero de cartas'), ('Pokemon', 'primero'), ('Atletismo', 'tiempo');
+VALUES ('Hearthstone', 'numero de cartas'), ('Pokemon', 'primero'), ('Atletismo', 'prueba');
 
 
 INSERT INTO Jugador (nombre) VALUES ('Maria'), ('Lucia'), ('Paco');
@@ -17,7 +17,7 @@ INSERT INTO juega (nombreJugador, idPartida, puntuacion, fecha)
 VALUES ('Maria', 0, 0, '01/12/2017'), ('Maria', 1, 1, '03/12/2017'), ('Maria', 2, 2, '05/12/2017'), ('Maria', 4, 3, '10/12/2017');
 
 INSERT INTO juega (nombreJugador, idPartida, puntuacion, fecha)
-VALUES ('Lucia', 0, 1, '01/12/2017'), ('Lucia', 3, 0, '07/12/2017'), ('Lucia', 4, 2, '10/12/2017');
+VALUES ('Lucia', 0, 1, '01/12/2017'), ('Lucia', 3, 40, '07/12/2017'), ('Lucia', 4, 2, '10/12/2017');
 
 INSERT INTO juega (nombreJugador, idPartida, puntuacion, fecha)
 VALUES ('Paco', 1, 0, '03/12/2017'), ('Paco', 2, 2, '05/12/2017'), ('Paco', 4, 1, '10/12/2017');
@@ -32,7 +32,7 @@ VALUES (0, 'Maria', 'primero', 'pikachu'),
 
 INSERT INTO rellena (idPartida, nombreJugador, nombreAtributo, valor)
 VALUES (0, 'Lucia', 'primero', 'bulbasaur'),
-(3, 'Lucia', 'tiempo', '01:53'),
+(3, 'Lucia', 'prueba', '100m'),
 (4, 'Lucia', 'primero', 'pikachu');
 
 
